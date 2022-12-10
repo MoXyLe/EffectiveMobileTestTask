9ca7d7da85babc1a652c44a1844b50da112b1708
MoXyLe <roofusmoxy@gmail.com>
2022-12-11 01:33:41 +0700

    Initial Commit

---
+++ EffectiveMobileTestTask/EffectiveMobileTestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  EffectiveMobileTestTaskApp.swift
+//  EffectiveMobileTestTask
+//
+//  Created by Виктор Шималин on 11.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct EffectiveMobileTestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
